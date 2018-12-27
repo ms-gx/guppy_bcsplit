5516d1a8daaf53fc509415ccc6dd497490277f8b
Michael Schmid <michael.schmid@genexa.ch>
2018-12-27 18:02:55 +0000

    update guppy_bsplit.py

--- guppy_bsplit.py
+++ guppy_bsplit.py
@@ -1,5 +1,13 @@
 import sys
+import os
 import csv
+import pathlib
+from Bio import SeqIO
+
+
+def touch(path):
+    with open(path, 'w'):
+        os.utime(path, None)
 
 if(len(sys.argv) < 4):
 	sys.stderr.write('not enough parameters\n')	
@@ -20,11 +28,20 @@ with open(barcodes_file) as csvfile:
 			my_stats[row[1]] += 1
 		else:
 			my_stats[row[1]] = 1	
-	read_to_barcode[row[0]] = row[1]
+		read_to_barcode[row[0]] = row[1]
 
 for x in sorted(my_stats):
 	print (x+':'+str(my_stats[x]))
 
 for x in my_stats:
 	current_barcode = x
-	pathlib.Path(prefix + '_' + current_barcode + '.fastq').touch()	
+	touch(prefix + '_' + current_barcode + '.fastq')	
+	#pathlib.Path(prefix + '_' + current_barcode + '.fastq').unlink()
+	#pathlib.Path(prefix + '_' + current_barcode + '.fastq').touch()	
+
+fastq_parser = SeqIO.parse(fastq_file, "fastq") 
+for fastq_rec in fastq_parser:     
+	read_name = fastq_rec.id
+	current_barcode = read_to_barcode[read_name]
+	with open(prefix + '_' + current_barcode + '.fastq', 'a') as fastq_handle:
+		SeqIO.write(fastq_rec, fastq_handle, "fastq")
